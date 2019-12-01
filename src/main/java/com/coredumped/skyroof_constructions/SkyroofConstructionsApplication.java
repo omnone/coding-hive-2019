634606a2e64684a76e4394cede3d02a83b93bd26
Ilias Nikas <ilnikas@protonmail.com>
2019-12-01 19:56:32 +0200

    Project structure initialized
    Three packets were created: dao
                                model
                                web
    Everything is below coredumped.skyroof_constructions

---
+++ src/main/java/com/coredumped/skyroof_constructions/SkyroofConstructionsApplication.java
@@ -0,0 +1,13 @@
+package com.coredumped.skyroof_constructions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyroofConstructionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyroofConstructionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coredumped/skyroof_constructions/SkyroofConstructionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coredumped.skyroof_constructions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyroofConstructionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
