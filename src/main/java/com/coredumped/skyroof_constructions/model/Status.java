c3e9c0c1ef9ca39ad58b948fd2f344ae4cbe4820
Ilias Nikas <ilnikas@protonmail.com>
2019-12-03 12:53:10 +0200

    Added entity for Status table

---
+++ src/main/java/com/coredumped/skyroof_constructions/model/Status.java
@@ -0,0 +1,45 @@
+package com.coredumped.skyroof_constructions.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Status_", schema = "skyroof_constructions", catalog = "")
+public class Status {
+    private byte statusId;
+    private String description;
+
+    @Id
+    @Column(name = "statusID")
+    public byte getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(byte statusId) {
+        this.statusId = statusId;
+    }
+
+    @Basic
+    @Column(name = "description_")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Status status = (Status) o;
+        return statusId == status.statusId &&
+                Objects.equals(description, status.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(statusId, description);
+    }
+}
