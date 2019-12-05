5d2eab30538c2f1693776fa04f8474d59daa1927
Kostas Bourantas <bourantas@ceid.upatras.gr>
2019-12-05 14:11:36 +0200

    Added basic security for authentication , temporary disabled

---
+++ src/main/java/com/coredumped/skyroof_constructions/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.coredumped.skyroof_constructions.services;
+
+import com.coredumped.skyroof_constructions.dao.UserDao;
+import com.coredumped.skyroof_constructions.model.User;
+import com.coredumped.skyroof_constructions.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional <User> user = userDao.findByusername(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(CustomUserDetails::new).get();
+    }
+}
