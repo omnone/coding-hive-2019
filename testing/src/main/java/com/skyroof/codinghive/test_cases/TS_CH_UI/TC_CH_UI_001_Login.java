206713fbba4eea7d17a31561c12b349314a92202
Ilias Nikas <ilnikas@protonmail.com>
2019-12-16 21:58:58 +0200

    Test file structure created and login test case implemented
    Currently configured for Linux

---
+++ testing/src/main/java/com/skyroof/codinghive/test_cases/TS_CH_UI/TC_CH_UI_001_Login.java
@@ -0,0 +1,22 @@
+package com.skyroof.codinghive.test_cases.TS_CH_UI;
+
+import com.skyroof.codinghive.test_utils.BaseTest;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class TC_CH_UI_001_Login extends BaseTest {
+
+    @Test
+    public void TC_CH_UI_001_Login(){
+        //input username
+        driver.findElement(By.id("username")).sendKeys("user1");
+        //input password
+        driver.findElement(By.id("password")).sendKeys("kodikos");
+        //submit
+        driver.findElement(By.name("submit")).click();
+
+        waitForLoad();
+        Assert.assertTrue(isElementPresent(By.id("nested-list-subheader")));
+    }
+}
